49b4c2b261f954d59adb8ffb6696fd47d2c9dd12
Philipp Katz <philipp@philippkatz.de>
2018-03-21 21:17:58 +0100

    TS typing for async handler function

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,8 @@
-declare module 'express-async-handler'
+import express = require('express');
+
+declare function expressAsyncHandler(handler: express.RequestHandler): express.RequestHandler;
+declare namespace expressAsyncHandler {
+
+}
+
+export = expressAsyncHandler;
