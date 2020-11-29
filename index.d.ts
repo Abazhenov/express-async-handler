7d85897588aa4dbd1a4576c182c81e2d2b46c7a7
Philipp Katz <philipp@philippkatz.de>
2020-11-30 00:47:59 +0100

    Return the appropriate express.RequestHandler type

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 import express = require('express');
 
-declare function expressAsyncHandler(handler: (...args: Parameters<express.RequestHandler>) => void | Promise<void>): express.RequestHandler;
+declare function expressAsyncHandler<R extends express.RequestHandler>(handler: (...args: Parameters<R>) => void | Promise<void>): R;
 declare namespace expressAsyncHandler {
 
 }
